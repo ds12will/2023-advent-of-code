790c9c5aceda2bd4db0a79af93e83dc28409f894
Dave Sadlon <36975918+ds12will@users.noreply.github.com>
2023-12-04 10:58:38 -0500

    Lots of dumb changes

--- Archive/Day02/Program.cs
+++ Archive/Day02/Program.cs
@@ -1,4 +1,4 @@
-/* using System.Runtime.CompilerServices;
+﻿/* using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
@@ -55,10 +55,4 @@ int FindAndPrintPrefix(string input, string targetString)
 
     return biggestSize;
 }
-/* 
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green */
  */
---
+++ Archive/Day03/Program.cs
@@ -0,0 +1,60 @@
+﻿/* using System.Text.RegularExpressions;
+
+record Pos(int Row, int ColStart, int ColEnd)
+    {
+        public bool IsAdjacent(Number number)
+        {
+            // same row, directly above or below
+            if (Row < number.Pos.Row - 1 || Row > number.Pos.Row + 1)
+                return false;
+            // within or directly adjacent to the number's column range
+            return ColStart >= number.Pos.ColStart - 1 && ColStart <= number.Pos.ColEnd + 1;
+        }
+    };
+
+    record Symbol(string Value, Pos Pos);
+
+    record Number(int Value, Pos Pos);
+
+var inputLines = File.ReadLines("Day3.txt");
+
+        var symbols = new List<Symbol>();
+        var numbers = new List<Number>();
+
+        var numberRegex = new Regex(@"(\d)+");
+        var symbolRegex = new Regex(@"[^\.\d\n]");
+
+        var lineNum = 0;
+        foreach (var line in inputLines)
+        {
+            foreach (Match nm in numberRegex.Matches(line))
+                numbers.Add(new Number(
+                    Value: int.Parse(nm.Value),
+                    Pos: new Pos(lineNum, nm.Index, nm.Index + nm.Length - 1)));
+
+            foreach (Match sm in symbolRegex.Matches(line))
+                symbols.Add(new Symbol(
+                    Value: sm.Value,
+                    Pos: new Pos(lineNum, sm.Index, sm.Index)));
+
+            lineNum++;
+        }
+
+        var partSum = numbers
+            .Where(n => symbols.Any(s => s.Pos.IsAdjacent(n)))
+            .Sum(n => n.Value);
+
+        var ratio = symbols
+            .Where(s => s.Value is "*")
+            .Sum(g =>
+            {
+                var adjacentNumbers = numbers
+                    .Where(n => g.Pos.IsAdjacent(n))
+                    .ToList();
+                return adjacentNumbers.Count == 2
+                    ? adjacentNumbers.Aggregate(1, (acc, n) => acc * n.Value)
+                    : 0;
+            });
+
+        Console.WriteLine($"Part 1 answer: {partSum}");
+        Console.WriteLine($"Part 2 answer: {ratio}"); */
